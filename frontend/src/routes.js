76d6d02b6a75794044218fe291ebffb560a9b789
wzhuoqun07 <93798637+wzhuoqun07@users.noreply.github.com>
2022-11-26 19:11:52 -0500

    add some pages
    add some nav pages 1-2 3-4 5-6

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -9,16 +9,21 @@ import PageLayout from "./components/PageLayout";
 import Home from "./pages/Home"
 import About from "./pages/About";
 import Error from "./pages/Error";
-
+import Contact from "./pages/Contact"
 import Lessons from "./pages/Lessons";
 import LessonPage from "./pages/LessonPage"
+
+import Grade12 from "./pages/Grade12";
+import Grade34 from "./pages/Grade34";
+import Grade56 from "./pages/Grade56";
+
 
 const router = createBrowserRouter(
   createRoutesFromElements([
     // root pages:
     <Route path="/" element={<PageLayout />}>
       <Route index element={<Home />} />
-      
+      <Route path="/contact" element={<Contact />} />
       <Route path="/about" element={<About />} />
       <Route path="/lessons" element={<Lessons />} />
       {/* individual lessons: */}
@@ -26,6 +31,10 @@ const router = createBrowserRouter(
       {/* /lessons/ */}
       {/* error page: */}
       <Route path="*" element={<Error />} />
+
+      <Route path={"/Lesson/Grade1_2"} element={<Grade12 />}/>
+      <Route path={"/Lesson/Grade3_4"} element={<Grade34 />}/>
+      <Route path={"/Lesson/Grade5_6"} element={<Grade56 />}/>      
     </Route>,
 
     // full pages:
