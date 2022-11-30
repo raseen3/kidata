1862f7e428a1393653b6c2d423382ab2b1d02aa3
golden2402 <golden2402@vt.edu>
2022-11-29 19:55:56 -0500

    add status class for each grade

--- frontend/src/pages/Lessons.js
+++ frontend/src/pages/Lessons.js
@@ -18,17 +18,17 @@ function Lessons() {
         <h1>Menu</h1>
         <ul>
           <NavLink to={"/lessons/grade1_2"}>
-            <li>
+            <li className="success">
               <h3>Grade 1-2</h3>
             </li>
           </NavLink>
           <NavLink to={"/lessons/grade3_4"}>
-            <li>
+            <li className="warning">
               <h3>Grade 3-4</h3>
             </li>
           </NavLink>
           <NavLink to={"/lessons/grade5_6"}>
-            <li>
+            <li className="danger">
               <h3>Grade 5-6</h3>
             </li>
           </NavLink>
